8f4299892b995a6d4792727a6432129f64af5b6e
yk <yuhei_kajima@nuco.co.jp>
2019-03-28 15:03:05 +0900

    arrange manage.py

--- flaskr/views.py
+++ flaskr/views.py
@@ -2,10 +2,12 @@ from flask import request, redirect, url_for, render_template, flash
 from flaskr import app, db
 from flaskr.models import Entry
 
+
 @app.route('/')
 def show_entries():
   entries = Entry.query.order_by(Entry.id.desc()).all()
   return render_template('show_entries.html', entries=entries)
+
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -17,3 +19,20 @@ def add_entry():
   db.session.commit()
   flash('New entry was successfully posted')
   return redirect(url_for('show_entries'))
+
+@app.route('/users/')
+def user_list():
+  return 'list users'
+
+@app.route('/users/<int:user_id>')
+def user_detail(user_id):
+  return 'detail user ' + str(user_id)
+
+@app.route('/users/<int:user_id>/edit/', methods=['GET, POST'])
+def uesr_edit(user_id):
+  return 'edit user ' + str(user_id)
+
+@app.route('/users/create/', methods=['GET', 'POST'])
+def user_create():
+  return 'create a new user'
+
